11d3db124d358bd06e9691896fbf98678924de94
gitnaman <naman.jain@1689@gmail.com>
2024-02-26 17:53:07 +0530

    gke creation with tf - 1st code

---
+++ provider.tf
@@ -0,0 +1,23 @@
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs
+provider "google" {
+  project = "friendly-storm-412108"
+  region  = "us-central1"
+}
+
+
+#https://www.terraform.io/language/settings/backends/gcs
+terraform {
+  backend "gcs" {
+    bucket = "gke_tfstore"
+    prefix = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+ }
+
+
